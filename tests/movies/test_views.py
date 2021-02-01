48ac7d288b7f9bb06dcf4f54131e8657d50079b9
Elisa Rocha <ecdgrocha@gmail.com>
2021-02-01 15:44:52 -0300

    chapter 13a

--- tests/movies/test_views.py
+++ tests/movies/test_views.py
@@ -1,5 +1,3 @@
-import json
-
 import pytest
 
 from movies.models import Movie
@@ -17,7 +15,7 @@ def test_add_movie(client):
             "genre": "comedy",
             "year": "1998",
         },
-        content_type="application/json"
+        content_type="application/json",
     )
     assert resp.status_code == 201
     assert resp.data["title"] == "The Big Lebowski"
@@ -25,16 +23,13 @@ def test_add_movie(client):
     movies = Movie.objects.all()
     assert len(movies) == 1
 
+
 @pytest.mark.django_db
 def test_add_movie_invalid_json(client):
     movies = Movie.objects.all()
     assert len(movies) == 0
 
-    resp = client.post(
-        "/api/movies/",
-        {},
-        content_type="application/json"
-    )
+    resp = client.post("/api/movies/", {}, content_type="application/json")
     assert resp.status_code == 400
 
     movies = Movie.objects.all()
@@ -52,7 +47,7 @@ def test_add_movie_invalid_json_keys(client):
             "title": "The Big Lebowski",
             "genre": "comedy",
         },
-        content_type="application/json"
+        content_type="application/json",
     )
     assert resp.status_code == 400
 
@@ -81,3 +76,69 @@ def test_get_all_movies(client, add_movie):
     assert resp.status_code == 200
     assert resp.data[0]["title"] == movie_one.title
     assert resp.data[1]["title"] == movie_two.title
+
+
+@pytest.mark.django_db
+def test_remove_movie(client, add_movie):
+    movie = add_movie(title="The Big Lebowski", genre="comedy", year="1998")
+
+    resp = client.get(f"/api/movies/{movie.id}/")
+    assert resp.status_code == 200
+    assert resp.data["title"] == "The Big Lebowski"
+
+    resp_two = client.delete(f"/api/movies/{movie.id}/")
+    assert resp_two.status_code == 204
+
+    resp_three = client.get("/api/movies/")
+    assert resp_three.status_code == 200
+    assert len(resp_three.data) == 0
+
+
+@pytest.mark.django_db
+def test_remove_movie_incorrect_id(client):
+    resp = client.delete(f"/api/movies/99/")
+    assert resp.status_code == 404
+
+
+@pytest.mark.django_db
+def test_update_movie(client, add_movie):
+    movie = add_movie(title="The Big Lebowski", genre="comedy", year="1998")
+
+    resp = client.put(
+        f"/api/movies/{movie.id}/",
+        {"title": "The Big Lebowski", "genre": "comedy", "year": "1997"},
+        content_type="application/json",
+    )
+    assert resp.status_code == 200
+    assert resp.data["title"] == "The Big Lebowski"
+    assert resp.data["year"] == "1997"
+
+    resp_two = client.get(f"/api/movies/{movie.id}/")
+    assert resp_two.status_code == 200
+    assert resp_two.data["title"] == "The Big Lebowski"
+    assert resp.data["year"] == "1997"
+
+
+@pytest.mark.django_db
+def test_update_movie_incorrect_id(client):
+    resp = client.put(f"/api/movies/99/")
+    assert resp.status_code == 404
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "add_movie, payload, status_code",
+    [
+        ["add_movie", {}, 400],
+        ["add_movie", {"title": "The Big Lebowski", "genre": "comedy"}, 400],
+    ],
+    indirect=["add_movie"],
+)
+def test_update_movie_invalid_json(client, add_movie, payload, status_code):
+    movie = add_movie(title="The Big Lebowski", genre="comedy", year="1998")
+    resp = client.put(
+        f"/api/movies/{movie.id}/",
+        payload,
+        content_type="application/json",
+    )
+    assert resp.status_code == status_code
